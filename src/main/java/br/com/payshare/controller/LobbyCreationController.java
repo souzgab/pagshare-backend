eb4dd6883a766409d991a3e9275b3309e5417299
unknown <gs_arruda@outlook.com>
2020-09-28 22:04:42 -0300

    update

--- src/main/java/br/com/payshare/controller/LobbyCreationController.java
+++ src/main/java/br/com/payshare/controller/LobbyCreationController.java
@@ -1,6 +1,8 @@
 package br.com.payshare.controller;
 
+import br.com.payshare.model.Lobby;
 import br.com.payshare.model.UserPf;
+import br.com.payshare.repository.LobbyRepository;
 import br.com.payshare.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,16 +16,32 @@ public class LobbyCreationController {
     @Autowired
     private UserRepository user;
 
+    @Autowired
+    private LobbyRepository lobbyRepository;
+
     @PostMapping(value = "/clients")
     public ResponseEntity createUser(@RequestBody UserPf userPf) {
         user.save(userPf);
         return ResponseEntity.created(null).build();
     }
 
-    @GetMapping
+    @PostMapping(value = "/lobby")
+    public ResponseEntity createUser(@RequestBody Lobby lobby, @RequestParam long id) {
+        lobbyRepository.save(lobby);
+        return ResponseEntity.created(null).build();
+    }
+
+    @GetMapping("/users")
     public ResponseEntity<?> getUser(){
         return new ResponseEntity<>(
                 user.findAll(), HttpStatus.OK
+        );
+    }
+
+    @GetMapping("/lobby")
+    public ResponseEntity<?> getLobby(){
+        return new ResponseEntity<>(
+                lobbyRepository.findAll(), HttpStatus.OK
         );
     }
 
