981e792f4aac2999c110878b9f591f22e34ba85d
Vinicius Alves Rodrigues da Silva <vinicius.silva@yandeh.com.br>
2020-11-18 18:42:25 -0300

    atualizando transaction

--- src/main/java/br/com/payshare/model/Transaction.java
+++ src/main/java/br/com/payshare/model/Transaction.java
@@ -16,7 +16,7 @@ public class Transaction implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "TRANSACTION_ID" , nullable = false)
     private long transactionId;
 
